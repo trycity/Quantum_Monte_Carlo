9817bc1ea69470aa628c302ef032380a91436a67
ikhan <trycity@gmail.com>
2017-09-20 09:51:26 -0400

    created util functions

---
+++ src/utils/QMC_Utilities.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file QMC_Utilities.cpp
+ * @author ikhan
+ * 
+ * @brief File contains implementation of utility methods used for quantum monte carlo project
+ */
+ 
+#include <valarray>
+#include <cmath>
+
+#include "QMC_Utilities.h"
+
+namespace QMC
+{
+namespace UTILS
+{
+
+double QMC_Distance(const QMCPoint& aPointA, const QMCPoint& aPointB)
+{
+   QMCPoint X = aPointA - aPointB;
+   std::valarray<double> Y = X*X;
+   
+   return sqrt(Y.sum());
+}
+
+}; // end namespace UTILS
+}; // end namespace QMC
+
+
+
---
+++ src/utils/QMC_Utilities.h
@@ -0,0 +1,34 @@
+/**
+ * @file QMC_Utilities.h
+ * @author ikhan
+ * 
+ * @brief File contains utility methods used for quantum monte carlo project
+ */
+ 
+#ifndef QMC_UTILITIES_H
+#define QMC_UTILITIES_H
+
+#include "QMC_Typedefs.h"
+
+namespace QMC
+{
+namespace UTILS
+{
+
+/**
+ * Method computes the distance between two points
+ *
+ * @param aPointA a QMC point
+ * @param aPointB a QMC point
+ *
+ * @return The distance between two points
+ */
+double QMC_Distance(const QMCPoint& aPointA, const QMCPoint& aPointB);
+
+
+}; // end namespace UTILS
+}; // end namespace QMC
+
+
+#endif // QMC_UTILITIES_H
+
